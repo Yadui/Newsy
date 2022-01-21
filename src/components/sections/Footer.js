f8a2410768f88415ef569260cd07b02e75347f88
DevDK12 <devdk2208@gmail.com>
2022-01-21 21:54:22 +0530

    Footer v2

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -6,22 +6,43 @@ const Footer = () => {
             <footer class="footer">
                 <div class="footer-container container-center">
                     <h3 class="logo">Logo</h3>
-                    <div class="footer-share-quote">
-                        <h5>Share, Get Good karma , Get more karma, Repeat.</h5>
-                    </div>
                     <div class="footer-info">
-                        <div class="footer-location">
+                        <div class="footer-developer">
                             <h4>Developed by</h4>
                             <p>Dev kumar</p>
                             <p>Abhinav</p>
+                            <p>Dr Sushil</p>
+                            <p>Abhinav</p>
                         </div>
                         <div class="footer-share">
-                            <h4>Share logos here</h4>
-                            <p>Some logos goes here</p>
+                            <div class="footer-share-quote">
+                                <h4>Share, Get Good karma , Get more karma, Repeat.</h4>
+                            </div>
+                            <p>Reminder : change icons here , Use svg not png</p>
+                            <div class="footer-logo-flex-container">
+                                <div class="footer-logos">
+                                    <div class="footer--logo">
+                                        {/* <a href="#"><img src="./images/instagram.png" alt="Instagram Icon"></a> */}
+                                    </div>
+                                    
+                                    <div class="footer--logo">
+                                        {/* <a href="#"><img src="./images/facebook.png" alt="Facebook Icon"></a> */}
+                                    </div>
+                                    
+                                    <div class="footer--logo">
+                                        {/* <a href="#"><img src="./images/linkedin.png" alt="LinkedIn Icon"></a> */}
+                                    </div>
+                                    
+                                    <div class="footer--logo">
+                                        {/* <a href="#"><img src="./images/twitter.png" alt="Twitter Icon"></a> */}
+                                    </div>
+                                    
+                                </div>
+                            </div>
                         </div>
                         <div class="footer-about">
                             <h4>About us</h4>
-                            <p>This is about us</p>
+                            <p>At the heart of our company is a global online community, where millions of people and thousands of political, cultural and commercial organisations engage in a continuous conversation about their beliefs, behaviours and brands.</p>
                         </div>
                     </div>
                     <div class="copyright">
