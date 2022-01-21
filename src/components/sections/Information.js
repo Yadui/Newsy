0de6151430a19811896d04f0b17e1db9a73f3e6c
DevDK12 <devdk2208@gmail.com>
2022-01-21 22:02:10 +0530

    Information section v2

--- src/components/sections/Information.js
+++ src/components/sections/Information.js
@@ -8,10 +8,31 @@ const Information = () => {
                     <div class="instructions">
                         <h2>Discover how YouGov can help you</h2>
                         <div class="inst-cards">
-                            <div class="inst--card">Instruction 1</div>
-                            <div class="inst--card">Instruction 2</div>
-                            <div class="inst--card">Instruction 3</div>
+                            <div class="inst--card">
+                                <div class="inst-logo">
+                                    <img src="./images/category.png" alt="" />
+                                </div>
+                                <h4 class="inst--head">News By Categories</h4>
+                                <p class="inst--content">Business, Entertainement, General, Health, Souce, Sports, Technology</p>
+                            </div>
+                            <div class="inst--card">
+                                <div class="inst-logo">
+                                    <img src="./images/business.png" alt="" />
+                                </div>
+                                <h4 class="inst--head">News By Terms</h4>
+                                <p class="inst--content">Bitcoin, Playstation 5, Smartphones, Donald Trump</p>
+                                
+                            </div>
+                            <div class="inst--card">
+                                <div class="inst-logo">
+                                    <img src="./images/news.png" alt="" />
+                                </div>
+                                <h4 class="inst--head">News by Sources</h4>
+                                <p class="inst--content">CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News</p>
+
+                            </div>
                         </div>
+                        
                     </div>
                 </div>
             </div>
