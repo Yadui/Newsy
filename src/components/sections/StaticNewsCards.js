6d45460a495df52670441f09bc8d1e1c064735d7
DevDK12 <devdk2208@gmail.com>
2022-01-23 11:49:02 +0530

    fetched news objects in static news cards and rendered using states

--- src/components/sections/StaticNewsCards.js
+++ src/components/sections/StaticNewsCards.js
@@ -1,59 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import StaticCard from './StaticCard';
 
 const StaticNewsCards = () => {
+    const [articles, setArticles] = useState([]);
 
+    const API_KEY = "6c0701cd55ef4880a5e369dc9a95311f";
+    let NEWS_API_URL =
+    "https://newsapi.org/v2/top-headlines?country=in&apiKey=" + API_KEY;
 
-let articles = []
+    fetch(NEWS_API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+        const newArticles = [];
+        // For getting entire array of news objects
+        // data.articles.map((article) => newArticles.push(article));
 
-
-    
-const API_KEY = '6c0701cd55ef4880a5e369dc9a95311f';
-let NEWS_API_URL = 'https://newsapi.org/v2/top-headlines?country=in&apiKey='+API_KEY;
-
-
-
-fetch(NEWS_API_URL)
-        .then(res => res.json()
-        )
-        .then(data => {
-            
-            for(var i = 0; i<1; i++){
-                // console.log(data.articles[i]);
-                articles[i] = data.articles[i];
-            }
-        })
-        .catch(error => console.log('Error'))
-
-
-
-console.log("Printing fetched objects now")
-console.log(articles.source)
-
-// articles.map((article) => {
-//     console.log(article.content)
-// })
-
-
-
-
+        // For getting only 3 news object 
+        for(let i=0; i<3; i++){
+            newArticles.push(data.articles[i]);
+        }
+        setArticles(newArticles);
+        console.log(articles);
+    })
+    .catch((error) => console.log("Error"));
 
 
     return (
         <div>
             <div id="all-cards" class="all-cards">
                 {articles.map((article) => {
-                    {/* console.log(element) */}
-                    return(
+                    return (
                         <div class="card">
                             <div class="card-image">
-                                {/* Need to set width here only for online pics and 100% wrt parent container */}
-                                <img src={article.urlToImage} width="100%" alt="News image" />
+                            {/* Need to set width here only for online pics and 100% wrt parent container */}
+                            <img src={article.urlToImage} width="100%" alt="News image" />
                             </div>
                             <div class="card-body">
                                 <div class="card-info">
-                                    <div class="card-date">{(new Date(article.publishedAt)).toDateString()}</div>
+                                    <div class="card-date">{new Date(article.publishedAt).toDateString()}</div>
                                     <div class="card-source">{article.source.name}</div>
                                 </div>
                                 <div class="card-title">
@@ -65,7 +49,7 @@ console.log(articles.source)
                                 <a href={article.url} target="_blank" class="btn card-btn">LEARN MORE</a>
                             </div>
                         </div>
-                    )
+                    );
                 })}
             </div>
         </div>
