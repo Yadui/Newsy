a8d1eb279f4208a6c63c4e70c5eb6e45e3b21597
DevDK12 <devdk2208@gmail.com>
2022-01-26 02:21:49 +0530

    added margin in news card of alan

--- src/components/NewCards/styles.js
+++ src/components/NewCards/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles({
     container: {
-        padding: '0 5%',
+        padding: '10% 5%',
         width: '100%',
         margin: 0,
     },
